dff9f5e7f4de6ee67d89066dadfd5664454a897a
Michael Birch <mbirch321@gmail.com>
2019-07-20 16:28:10 +1200

    Fix deprecation

--- src/Controller/SpongeUsersController.php
+++ src/Controller/SpongeUsersController.php
@@ -127,7 +127,7 @@ class SpongeUsersController extends AppController
         // add the next line to allow role to be edited
         // see https://stackoverflow.com/questions/44912295/cakedc-users-new-role
         // and https://github.com/CakeDC/users/issues/513
-        $entity->role = $this->request->data('role');
+        $entity->role = $this->request->getData('role');
         $singular = Inflector::singularize(Inflector::humanize($tableAlias));
         if ($table->save($entity)) {
             $this->Flash->success('The user has been saved');
